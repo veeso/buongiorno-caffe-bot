14f7102a3765dd9135d9821bf107d0a353fa6bf7
veeso <christian.visintin1997@gmail.com>
2022-09-02 15:37:20 +0200

    automatizer

--- src/bot/automatize.rs
+++ src/bot/automatize.rs
@@ -2,10 +2,13 @@
 //!
 //! A module to automatize messages
 
+use crate::buongiornissimo::Media;
+
 use super::repository::Repository;
 use super::AnswerBuilder;
+use crate::utils::random as random_utils;
 
-use chrono::{Local, NaiveDate};
+use chrono::{Datelike, Local, NaiveDate};
 use teloxide::prelude::*;
 use teloxide::types::ChatId;
 use thiserror::Error;
@@ -83,22 +86,100 @@ impl Automatizer {
     /// Setup cron scheduler
     async fn setup_cron_scheduler() -> AutomatizerResult<JobScheduler> {
         let sched = JobScheduler::new().await?;
-        // aphorism jobs
-        /*let morning_aphorism_job = Job::new_async("0 0 6 * * *", |_, _| {
+        // birthday job
+        let happy_birthday_job = Job::new_async("0 30 8 * * *", |_, _| {
             Box::pin(async move {
-                info!("running morning_aphorism_job");
-                if let Err(err) = Self::send_perla().await {
-                    error!("evening_aphorism_job failed: {}", err);
+                info!("running morning_job");
+                if let Err(err) = Self::send_happy_birthday().await {
+                    error!("happy_birthday_job failed: {}", err);
                 }
             })
-            sched.add(morning_aphorism_job).await?;
-        })?;*/
+        })?;
+        sched.add(happy_birthday_job).await?;
+        // aphorism jobs
+        let good_morning_job = Job::new_async("0 30 6 * * *", |_, _| {
+            Box::pin(async move {
+                info!("running morning_job");
+                if let Err(err) = Self::send_good_morning().await {
+                    error!("good_morning_job failed: {}", err);
+                }
+            })
+        })?;
+        sched.add(good_morning_job).await?;
+
+        let good_afternoon_job = Job::new_async("0 40 12 * * *", |_, _| {
+            Box::pin(async move {
+                info!("running afternoon_job");
+                if let Err(err) = Self::send_greeting(Media::BuonPomeriggio).await {
+                    error!("good_afternoon_job failed: {}", err);
+                }
+            })
+        })?;
+        sched.add(good_afternoon_job).await?;
+
+        let good_night_job = Job::new_async("0 40 12 * * *", |_, _| {
+            Box::pin(async move {
+                info!("running afternoon_job");
+                if let Err(err) = Self::send_greeting(Media::BuonaNotte).await {
+                    error!("good_night_job failed: {}", err);
+                }
+            })
+        })?;
+        sched.add(good_night_job).await?;
 
         sched
             .start()
             .await
             .map(|_| sched)
             .map_err(AutomatizerError::from)
+    }
+
+    /// Send happy birthday
+    async fn send_happy_birthday() -> anyhow::Result<()> {
+        let today_birthdays = Self::today_birthdays().await?;
+        if today_birthdays.is_empty() {
+            return Ok(());
+        }
+        let image = super::Buongiornissimo::get_buongiornissimo_image(Media::Auguri).await?;
+        let bot = Bot::from_env().auto_send();
+        for (chat, name, _) in today_birthdays.into_iter() {
+            if let Err(err) = AnswerBuilder::default()
+                .image(image.clone())
+                .text(format!("Buon compleanno {}!", name))
+                .finalize()
+                .send(&bot, chat)
+                .await
+            {
+                error!("failed to send happy birthday to {}: {}", chat, err);
+            }
+        }
+        Ok(())
+    }
+
+    /// Send good morning greeting
+    async fn send_good_morning() -> anyhow::Result<()> {
+        let media = match Local::today().naive_local() {
+            date if date == NaiveDate::from_ymd(date.year(), 12, 25) => Media::BuonNatale,
+            _ => *random_utils::choice(&[Media::BuonGiorno, Media::BuonGiornoWeekday]),
+        };
+        Self::send_greeting(media).await
+    }
+
+    /// Send generic greeting
+    async fn send_greeting(media: Media) -> anyhow::Result<()> {
+        let subscribed_chats = Self::subscribed_chats().await?;
+        if subscribed_chats.is_empty() {
+            return Ok(());
+        }
+        let greeting = super::Buongiornissimo::get_buongiornissimo_image(media).await?;
+        let answer = AnswerBuilder::default().image(greeting).finalize();
+        let bot = Bot::from_env().auto_send();
+        for chat in subscribed_chats.iter() {
+            if let Err(err) = answer.clone().send(&bot, *chat).await {
+                error!("failed to send scheduled greeting to {}: {}", chat, err);
+            }
+        }
+        Ok(())
     }
 
     pub async fn subscribed_chats() -> anyhow::Result<Vec<ChatId>> {
